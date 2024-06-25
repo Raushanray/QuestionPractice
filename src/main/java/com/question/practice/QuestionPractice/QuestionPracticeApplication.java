cc7ee74b80a8451f346d40c91965b94d922cded0
Raushanray <raushankuray1998@gmail.com>
2024-06-25 11:00:10 +0530

    create a project for Question Practices.

---
+++ src/main/java/com/question/practice/QuestionPractice/QuestionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.question.practice.QuestionPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/question/practice/QuestionPractice/QuestionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.question.practice.QuestionPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
